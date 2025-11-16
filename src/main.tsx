50d433e7f56b2bb75d5738b6a19d6a3c2e786827
Johnathan Yu <johnathanzyu@gmail.com>
2025-11-15 22:48:14 -0500

    Add carousel, reformat project, add style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/index.css";
+import "./css/embla.css";
+import App from "./ts/App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ src/ts/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "../css/App.css";
+import { type EmblaOptionsType } from "embla-carousel";
+import EmblaCarousel from "./EmblaCarousel";
+
+function App() {
+  const [count, setCount] = useState(3);
+  const OPTIONS: EmblaOptionsType = { loop: true };
+  const SLIDE_COUNT = 5;
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+
+  function testFunction() {
+    console.log("return value: " + count);
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 2)}>
+          count 1 is {count}
+        </button>
+        <button onClick={testFunction}>print text</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
